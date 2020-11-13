e1726c2ce0e81f266075e092afa979ebbdf25cf7
narges_sh_maleki <narges.sh.maleki@gmail.com>
2020-11-13 10:25:44 +0100

    rolled back the bom

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-11-06T14:14:44+0100",
+    date = "2020-11-13T10:16:40+0100",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.6 (Oracle Corporation)"
 )
 @Component
