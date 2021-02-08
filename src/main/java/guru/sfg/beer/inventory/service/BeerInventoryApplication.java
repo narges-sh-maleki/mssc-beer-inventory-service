1c6200ad0598a1f22403a81e9b541062692babd3
NargesMaleki <narges.sh.maleki@gmail.com>
2021-02-08 15:41:05 +0100

    remove spring.cloud.discovery.enabled=false, added spring.cloud.config.discovery.enabled=true

--- src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
+++ src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
@@ -1,13 +1,21 @@
 package guru.sfg.beer.inventory.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BeerInventoryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BeerInventoryApplication.class, args);
+    }
+
+    @Bean
+    public String getMyProfile(@Value("${spring.profiles.active:my default value}") String myProfile){
+        System.out.println("********************" + myProfile +"********************" );
+        return myProfile;
     }
 
 }
